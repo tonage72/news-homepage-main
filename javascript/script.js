67e78934c1075d4fbd266532ff93424c925e6d3d
David Fleming <tonage72@gmail.com>
2024-04-06 19:47:38 -0400

    zindex

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const navigation = document.querySelector("nav");
+const hamburgerClose = document.querySelector(".hamburger-close");
+const overlay = document.querySelector(".overlay");
+
+overlay.style.zIndex = "1";
+navigation.style.zIndex = "3";
 
 hamburgerMenu.addEventListener("click", (e) => {
-	hamburgerMenu.style.display = "none";
+	overlay.style.display = "block";
 	navigation.style.display = "block";
+});
+
+hamburgerClose.addEventListener("click", (e) =>{
+	navigation.style.display = "none";
+	overlay.style.display = "none";
 });
