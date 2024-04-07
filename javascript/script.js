55a6919cd24b0d46cb64198e2ef9b711611eafa0
David Fleming <tonage72@gmail.com>
2024-04-06 21:01:16 -0400

    Adding links

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,6 @@ const navigation = document.querySelector("nav");
 const hamburgerClose = document.querySelector(".hamburger-close");
 const overlay = document.querySelector(".overlay");
 
-overlay.style.zIndex = "1";
-navigation.style.zIndex = "3";
-
 hamburgerMenu.addEventListener("click", (e) => {
 	overlay.style.display = "block";
 	navigation.style.display = "block";
